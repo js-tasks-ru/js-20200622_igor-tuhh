8bc51c1f85517de7412e9ea5c8b0ee7ec9159e0d
ms-coder <igor.tuhh@gmail.com>
2020-06-29 12:11:06 +0200

    reworked

--- 02-javascript-data-types/1-sort-strings/index.js
+++ 02-javascript-data-types/1-sort-strings/index.js
@@ -4,43 +4,10 @@
  * @param {string} [param="asc"] param - the sorting type "asc" or "desc"
  * @returns {string[]}
  */
-export function sortStrings(arr, param = 'asc') {
-
+export function sortStrings(arr, param = "asc") {
   const clonedArray = [...arr];
-  const sortingType = param === 'desc' ? 1 : -1;
-  quickSort(clonedArray, 0, arr.length - 1);
-  return clonedArray;
+  const sortingFactor = param === "desc" ? -1 : 1;
+  const collator = new Intl.Collator(undefined, { caseFirst: "upper" });
 
-  function quickSort(array, low, high) {
-    if (low < high) {
-      const partition = getPartition(array, low, high);
-      quickSort(array, low, partition - 1);
-      quickSort(array, partition + 1, high);
-    }
-  }
-  
-  function getPartition(array, low, high) {
-    const pivot = array[high];
-    let i = low;
-    for (let j = low; j < high; j++) {
-      if (array[j].localeCompare(pivot, undefined, {caseFirst: "upper"}) === sortingType) {
-        swap(array, i, j);
-        i = i + 1;
-      }
-    }
-    
-    swap(array, i, high);
-    return i;
-  }
-    
-  function swap(array, i, j) {
-    const temp = array[i];
-    array[i] = array[j];
-    array[j] = temp;
-  }
+  return clonedArray.sort((a, b) => collator.compare(a, b) * sortingFactor);
 }
-
-
-
-//sortStrings(['абрикос', 'Абрикос', 'яблоко', 'Яблоко', 'ёжик', 'Ёжик'], "desc");
-
