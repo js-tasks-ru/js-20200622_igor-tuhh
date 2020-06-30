d4c7e1cad9406e5cb598a52df450ad76e6046b34
ms-coder <igor.tuhh@gmail.com>
2020-06-30 16:30:01 +0200

    createGetter implemented

--- 02-javascript-data-types/3-omit/index.js
+++ 02-javascript-data-types/3-omit/index.js
@@ -15,3 +15,5 @@ export const omit = (obj, ...fields) => {
 
   return result;
 };
+
+
--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,5 +4,13 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-
+  // array.splice is needed for performance reasons, so we make the array shorter at the place where no further nested properties could be found
+  return (obj) =>
+    path
+      .split(".")
+      .reduce(
+        (accumulator, currentValue, index, array) =>
+          !accumulator ? array.splice(index) : accumulator[currentValue], 
+        obj
+      );
 }
