1adb5e6cfb7e2c4ddf66daddf18417f83cc526b1
ms-coder <igor.tuhh@gmail.com>
2020-06-30 16:54:30 +0200

    fix for createGetter and inverting object

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -4,13 +4,14 @@
  * @returns {function} - function-getter which allow get value from object by set path
  */
 export function createGetter(path) {
-  // array.splice is needed for performance reasons, so we make the array shorter at the place where no further nested properties could be found
+  // array.splice is needed for performance reasons, so we make the array shorter at the place where no further nested properties could be found (it breaks reduce())
   return (obj) =>
-    path
-      .split(".")
-      .reduce(
-        (accumulator, currentValue, index, array) =>
-          !accumulator ? array.splice(index) : accumulator[currentValue], 
-        obj
-      );
+    path.split('.').reduce((accumulator, currentValue, index, array) => {
+      if (typeof accumulator !== 'object' || !accumulator[currentValue]) {
+        array.splice(index);
+        return undefined;
+      }
+
+      return accumulator[currentValue];
+    }, obj);
 }
